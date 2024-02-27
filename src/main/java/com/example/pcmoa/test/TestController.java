e96dc61ba41c5f95dd6a8b3c35bb00a4b5f55d8e
ChoiDevv <oinkoink71@naver.com>
2024-02-27 11:23:23 +0900

    CICD 테스트

---
+++ src/main/java/com/example/pcmoa/test/TestController.java
@@ -0,0 +1,17 @@
+package com.example.pcmoa.test;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${app.version:3.2.3}")
+    String version;
+
+    @GetMapping("/")
+    public String version() {
+        return "Welcome, CICD TEST! This spring boot version is " + version;
+    }
+}
