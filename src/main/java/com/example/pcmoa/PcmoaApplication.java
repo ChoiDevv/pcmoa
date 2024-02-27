6c900cdd60c381abbfdbc436ceb3dfe80984677e
ChoiDevv <oinkoink71@naver.com>
2024-02-27 10:50:34 +0900

    스프링 부트 서버 생성

---
+++ src/main/java/com/example/pcmoa/PcmoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pcmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcmoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcmoaApplication.class, args);
+	}
+
+}
